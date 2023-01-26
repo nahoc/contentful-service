31719c32eb9f6022c33356b4847ceb5f039cc4a5
Cohan Carpentier <cohan.carpentier@gmail.com>
2023-01-26 11:59:19 -0500

    fix: upload images

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
-
 require('dotenv').config()
+
 const app = require("express")();
 const contentful = require('contentful-management');
 const cors = require('cors');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const multer = require('multer')
+const upload = multer({
+  dest: 'uploads/'
+})
 
 const CONTENTFUL_ENVIRONMENT_ID = 'master';
 const CONTENTFUL_SPACE_ID = 'e2z03gbgxg1a';
@@ -10,98 +16,60 @@ const client = contentful.createClient({
   accessToken: process.env.VITE_CONTENTFUL_MANAGEMENT_TOKEN,
 })
 
+app.use(bodyParser.json());
+
 app.use(cors({
-  origin: '*'
+  origin: '*',
+  credentials: true
 }));
 
 app.get("/favicon.ico", (req, res) => {
   res.sendStatus(204);
 });
 
-app.post("/upload-image", async (req, res) => {
-  const files = req.params.files
-  const name = req.params.name
+app.post('/upload-image', upload.array('files'), async function (req, res, next) {
+  const file = req.files[0]
+  const fileStream = fs.createReadStream(`./uploads/${file.filename}`)
 
- await contentfulClient
+  await client
     .getSpace(CONTENTFUL_SPACE_ID)
     .then((space) => space.getEnvironment(CONTENTFUL_ENVIRONMENT_ID))
     .then((environment) =>
       environment.createAssetFromFiles({
         fields: {
           title: {
-            'en-US': files[0].name,
+            'en-US': file.originalname,
           },
           description: {
-            'en-US': name,
+            'en-US': file.originalname,
           },
           file: {
             'en-US': {
-              contentType: files[0].type,
-              fileName: files[0].name,
-              file: files[0],
+              contentType: file.mimetype,
+              fileName: file.originalname,
+              file: fileStream,
             },
           },
         },
       }),
     )
     .then((asset) => asset.processForAllLocales())
-    .then((asset) => asset.publish()).then(() => {
-      res.status(200)
+    .then((asset) => asset.publish()).then(() => {
+      // wipe file from uploads folder
+      fs.unlink(file.path, (err) => {
+        if (err) throw err;
+      });
 
+      res.status(200)
     })
-    .catch((err) => {
+    .catch((err) => {
       res.status(400).send(err)
     });
-    
-    res.end()
-}); 
- 
 
-app.get("/create", (req, res) => {
-  console.log(req.params)
-  //console.log(client)
-  client.getSpace(CONTENTFUL_SPACE_ID)
-  .then((space) => space.getEnvironments())
-  .then((response) => {
-    console.log(response.items)
-  })
-  .catch(console.error)
-  /*const { variant = DEFAULT_VARIANT, size = DEFAULT_SIZE } = req.params;
-  const explicitName = req.query.name || req.params.name;
-  const name = explicitName || Math.random().toString();
-  const colors = normalizeColors(req.query.colors || DEFAULT_COLORS);
-  const square = req.query.hasOwnProperty("square");
+  res.end()
+})
 
-  if (!VALID_VARIANTS.has(variant)) {
-    return res.status(400).send("Invalid variant");
-  }
-
-  res.setHeader("Content-Type", "image/svg+xml");
-
-  if (explicitName) {
-    res.setHeader("Cache-Control", "max-age=0, s-maxage=2592000");
-  } else {
-    res.setHeader("Cache-control", "max-age=0, s-maxage=1");
-  }
-
-  const svg = renderToString(
-    React.createElement(
-      Avatar,
-      {
-        size,
-        name,
-        variant,
-        colors,
-        square,
-      },
-      null
-    )
-  );
-
-  res.end(svg);*/
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server running on ${port}, http://localhost:${port}`);
