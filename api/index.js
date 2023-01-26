92a837c7b26ab7664b2ee8041cf163c985ac0587
Cohan Carpentier <cohan.carpentier@gmail.com>
2023-01-26 17:09:05 -0500

    fix: update endpoint

--- api/index.js
+++ api/index.js
@@ -72,7 +72,7 @@ app.post('/upload-image', upload.array('files'), async function (req, res, next)
 })
 
 // delete project endpoint
-app.delete('/delete-project/:id/:token', async function(req, res) {
+app.delete('/delete-project/:id/:token', async function (req, res) {
   const idToDelete = req.params.id
   const signature = req.params.token
 
@@ -92,6 +92,112 @@ app.delete('/delete-project/:id/:token', async function(req, res) {
       return entry.unpublish();
     })
     .then((entry) => entry.archive()) // we archive, but say we delete in case they really want to revert
+    .then(() => res.sendStatus(200))
+    .catch((err) => res.status(400).send(err))
+
+  res.end()
+})
+
+// update project endpoint
+app.post('/update-project/:id/:token', async function (req, res) {
+  const idToUpdate = req.params.id
+  const signature = req.params.token
+  const body = req.body
+
+  if (!signature) {
+    throw '';
+  }
+
+  await client
+    .getSpace(CONTENTFUL_SPACE_ID)
+    .then((space) => space.getEnvironment(CONTENTFUL_ENVIRONMENT_ID))
+    .then((environment) => environment.getEntry(idToUpdate))
+    .then((entry) => {
+      if (signature === entry.fields.signature?.['en-US'] && body.account === entry.fields.owner?.['en-US']) {
+        entry.fields.color = {
+          ['en-US']: body.data.color || entry.fields.color?.['en-US']
+        };
+        entry.fields.description = {
+          ['en-US']: body.data.description || entry.fields.description?.['en-US']
+        };
+        entry.fields.descriptionLong = {
+          ['en-US']: body.data.descriptionLong || entry.fields.descriptionLong?.['en-US']
+        };
+        entry.fields.discord = {
+          ['en-US']: body.data.discord || entry.fields.discord?.['en-US']
+        };
+        entry.fields.docs = {
+          ['en-US']: body.data.docs || entry.fields.docs?.['en-US']
+        };
+        entry.fields.github = {
+          ['en-US']: body.data.github || entry.fields.github?.['en-US']
+        };
+        entry.fields.medium = {
+          ['en-US']: body.data.medium || entry.fields.medium?.['en-US']
+        };
+        entry.fields.name = {
+          ['en-US']: body.data.name || entry.fields.name?.['en-US']
+        };
+        entry.fields.priceTracker = {
+          ['en-US']: body.data.priceTracker || entry.fields.priceTracker?.['en-US']
+        };
+        entry.fields.reddit = {
+          ['en-US']: body.data.reddit || entry.fields.reddit?.['en-US']
+        };
+        entry.fields.telegram = {
+          ['en-US']: body.data.telegram || entry.fields.telegram?.['en-US']
+        };
+        entry.fields.tokenContract = {
+          ['en-US']: body.data.tokenContract || entry.fields.tokenContract?.['en-US']
+        };
+        entry.fields.twitter = {
+          ['en-US']: body.data.twitter || entry.fields.twitter?.['en-US']
+        };
+        entry.fields.website = {
+          ['en-US']: body.data.website || entry.fields.website?.['en-US']
+        };
+        entry.fields.youtube = {
+          ['en-US']: body.data.youtube || entry.fields.youtube?.['en-US']
+        };
+        entry.fields.tags = {
+          ['en-US']: body.data.tagsIds.map((tagId) => ({
+            sys: {
+              type: 'Link',
+              linkType: 'Entry',
+              id: tagId,
+            },
+          })) || entry.fields.tags?.['en-US'],
+        };
+
+        if (body.data.bannerAsset) {
+          entry.fields.banner = {
+            ['en-US']: {
+              sys: {
+                id: body.data.bannerAsset.sys.id,
+                linkType: 'Asset',
+                type: 'Link'
+              }
+            }
+          };
+        }
+
+        if (body.data.avatarAsset) {
+          entry.fields.logo = {
+            ['en-US']: {
+              sys: {
+                id: body.data.avatarAsset.sys.id,
+                linkType: 'Asset',
+                type: 'Link'
+              }
+            }
+          };
+        }
+
+        return entry.update();
+      } else {
+        throw t('You need to be the owner of the project to request changes');
+      }
+    })
     .then(() => res.sendStatus(200))
     .catch((err) => res.status(400).send(err))
 
